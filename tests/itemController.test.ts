5b86694d3ba692473f6f0727756d0ca94ce31d2f
OpenSorcerer20 <harryjry@yahoo.com>
2025-05-12 14:20:39 -0500

    added unit tests

--- tests/itemController.test.ts
+++ tests/itemController.test.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
 
-import { getItems } from '../src/controllers/itemController';
+import {
+  createItem,
+  deleteItem,
+  getItems,
+  updateItem,
+} from '../src/controllers/itemController';
 import { items } from '../src/models/item';
 
 describe('Item Controller', () => {
@@ -21,4 +26,56 @@ describe('Item Controller', () => {
     // test that json was called with empty array
     expect(res.json).toHaveBeenCalledWith([]);
   });
+
+  it('should add an item when created', async () => {
+    const req = { body: { name: 'test one' } } as Request;
+    const res = {} as unknown as Response;
+    items.length = 0;
+    createItem(req, res, jest.fn());
+    expect(items.length).toEqual(1);
+    expect(items[0]?.name).toBe('test one');
+  });
+
+  it('should update an item', async () => {
+    const req1 = { body: { name: 'test one' } } as Request;
+    const res1 = {} as unknown as Response;
+
+    // create item
+    items.length = 0;
+    createItem(req1, res1, jest.fn());
+
+    // get id
+    getItems({} as Request, {} as unknown as Response, jest.fn());
+    // update item
+    const req2 = {
+      params: { id: items[0].id },
+      body: { name: 'test two' },
+    } as unknown as Request; // "as unknown as request" bypasses strict definitions for Request; otherwise i cant specify params like this
+    const res2 = {} as unknown as Response;
+
+    updateItem(req2, res2, jest.fn());
+    expect(items.length).toEqual(1);
+    expect(items[0]?.name).toBe('test two');
+  });
+
+  it('should delete an item', async () => {
+    const req1 = { body: { name: 'test one' } } as Request;
+    const req2 = { body: { name: 'test two' } } as Request;
+    const res1 = {} as unknown as Response;
+
+    // create items
+    items.length = 0;
+    createItem(req1, res1, jest.fn());
+    createItem(req2, res1, jest.fn());
+
+    // remove test one
+    const req3 = {
+      params: { id: items[0].id },
+    } as unknown as Request;
+    deleteItem(req3, res1, jest.fn());
+    expect(items.length).toBe(1);
+    expect(items[0]?.name).toBe('test two');
+  });
+
+  // @todo test getItem and getItems (need to test json return from route
 });
